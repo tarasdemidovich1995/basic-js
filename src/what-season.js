d635456fac4bb53b814935ceced817abbaed03e0
Taras Demidovich <tarasdemidovich1995@gmail.com>
2020-02-16 12:54:00 +0300

    feat: add solve for what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,19 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  let month = date.getMonth();
+  let timeOfYear;
+  switch (true) {
+    case month == 10 || month <= 1:
+      timeOfYear = 'winter';
+      break;
+    case month >= 2 && month <= 4:
+      timeOfYear = 'spring';
+      break;
+    case month >= 5 && month <= 7:
+      timeOfYear = 'summer';
+      break;
+    case month >= 8 && month <= 10:
+      timeOfYear = 'autumn';
+      break;
+  }
+  return timeOfYear;
 };
