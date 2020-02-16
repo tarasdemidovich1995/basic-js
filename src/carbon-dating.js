afbdd1500382f3897a843a147277a23f9959fc2b
Taras Demidovich <tarasdemidovich1995@gmail.com>
2020-02-16 12:23:37 +0300

    feat: add solve for carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,11 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let lifePeriod = 0;
+  while (sampleActivity > MODERN_ACTIVITY) {
+    lifePeriod += HALF_LIFE_PERIOD;
+    sampleActivity /= 2;
+  }
+  return lifePeriod == 0 ? false : lifePeriod;
 };
