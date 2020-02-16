5714e0334dafa718e5dd7b778f189c6b8e309410
Taras Demidovich <tarasdemidovich1995@gmail.com>
2020-02-16 13:13:17 +0300

    fix: fix bag in count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,7 +1,7 @@
 module.exports = function countCats(backyard) {
   let ears = 0;
   for (let i = 0; i < backyard.length; i++) {
-    for (let j = 0; j < row.length; j++) {
+    for (let j = 0; j < backyard[i].length; j++) {
       if (backyard[i][j] === '^^') ears++;
     }
   }
