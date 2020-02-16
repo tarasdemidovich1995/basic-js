9d2147c61cb78e76c2c25aee872ca1df0114fdea
Taras Demidovich <tarasdemidovich1995@gmail.com>
2020-02-16 12:08:03 +0300

    feat: add solve for count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,9 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let ears = 0;
+  for (let i = 0; i < backyard.length; i++) {
+    for (let j = 0; j < row.length; j++) {
+      if (backyard[i][j] === '^^') ears++;
+    }
+  }
+  return ears;
 };
