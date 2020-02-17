c6f402167b616e44f5b687115080f57b1567c2d3
Taras Demidovich <tarasdemidovich1995@gmail.com>
2020-02-17 14:49:26 +0300

    feat: add solution to extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,20 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let { repeatTimes, separator, addition, additionRepeatTimes, additionSeparator } = options;
+    let sep = separator ? separator : '+';
+    let add = typeof addition !== 'undefined' ? addition : '';
+    let addSep = additionSeparator ? additionSeparator : '';
+    let addBase = add;
+    if (additionRepeatTimes) {
+        for (let i = 1; i < additionRepeatTimes; i++) {
+            add += addSep + addBase;
+        }
+    }
+    let strBase = str + add;
+    str = strBase;
+    if (repeatTimes) {
+        for (let i = 1; i < repeatTimes; i++) {
+            str += sep + strBase;
+        }
+    }
+    return str;
 };
-  
