42affacc83c454d248e367632b013baa15566491
Taras Demidovich <tarasdemidovich1995@gmail.com>
2020-02-17 10:45:44 +0300

    feat: add solution to transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,53 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error();
+    }
+    let transArr = [];
+    let controlAction = null;
+    for (let i = 0; i < arr.length || controlAction; i++) {
+        if (controlAction) {
+            switch (controlAction) {
+                case '--double-next':
+                    if (i < arr.length) transArr.push(arr[i]);
+                    controlAction = null;
+                    break;
+                case '--double-prev':
+                    if (transArr.length != 0) transArr.push(transArr[transArr.length - 1]);
+                    controlAction = null;
+                    break;
+            }
+        }
+        if (isNaN(arr[i])) {
+            if (arr[i] == '--double-next' || arr[i] == '--double-prev') {
+                controlAction = arr[i];
+                continue;
+            }
+        }
+        if (i < arr.length) transArr.push(arr[i]);
+    }
+    arr = transArr;
+    transArr = [];
+    for (let i = 0; i < arr.length || controlAction; i++) {
+        if (controlAction) {
+            switch (controlAction) {
+                case '--discard-next':
+                    controlAction = null;
+                    continue;
+                    break;
+                case '--discard-prev':
+                    if (transArr.length != 0) transArr.pop();
+                    controlAction = null;
+                    break;
+            }
+        }
+        if (isNaN(arr[i])) {
+            if (arr[i] == '--discard-next' || arr[i] == '--discard-prev') {
+                controlAction = arr[i];
+                continue;
+            }
+        }
+        if (i < arr.length) transArr.push(arr[i]);
+    }
+
+    return transArr;
 };
