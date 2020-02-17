a22359be395e976386d0ac7a630a5491c9656fca
Taras Demidovich <tarasdemidovich1995@gmail.com>
2020-02-17 17:09:40 +0300

    feat: add solution for vigenere-copher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,84 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    shift = {
+        A: 0,
+        B: 1,
+        C: 2,
+        D: 3,
+        E: 4,
+        F: 5,
+        G: 6,
+        H: 7,
+        I: 8,
+        J: 9,
+        K: 10,
+        L: 11,
+        M: 12,
+        N: 13,
+        O: 14,
+        P: 15,
+        Q: 16,
+        R: 17,
+        S: 18,
+        T: 19,
+        U: 20,
+        V: 21,
+        W: 22,
+        X: 23,
+        Y: 24,
+        Z: 25,
     }
-
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(isStraight) {
+        if (typeof isStraight == 'undefined') {
+            this.isStraight = true;
+        } else {
+            this.isStraight = isStraight;
+        }
+    }
+    encrypt(message, key) {
+        if (typeof message == 'undefined' || typeof key == 'undefined') {
+            throw new Error();
+        }
+        key = key.toUpperCase();
+        let keySymbIndex = 0;
+        message = message.replace(/[a-z]/ig, match => {
+            if (keySymbIndex >= key.length) {
+                keySymbIndex = 0;
+            }
+            let shift = this.shift[key[keySymbIndex++]];
+            let newStrCode = match.toUpperCase().codePointAt(0) + shift;
+            if (newStrCode > 90) {
+                newStrCode = newStrCode - 26;
+            }
+            return String.fromCodePoint(newStrCode);
+        });
+        if (this.isStraight) {
+            return message;
+        } else {
+            return message.split('').reverse().join('');
+        }
+    }
+    decrypt(message, key) {
+        if (typeof message == 'undefined' || typeof key == 'undefined') {
+            throw new Error();
+        }
+        key = key.toUpperCase();
+        let keySymbIndex = 0;
+        message = message.replace(/[a-z]/ig, match => {
+            if (keySymbIndex >= key.length) {
+                keySymbIndex = 0;
+            }
+            let shift = this.shift[key[keySymbIndex++]];
+            let newStrCode = match.toUpperCase().codePointAt(0) - shift;
+            if (newStrCode < 65) {
+                newStrCode = newStrCode + 26;
+            }
+            return String.fromCodePoint(newStrCode);
+        });
+        if (this.isStraight) {
+            return message;
+        } else {
+            return message.split('').reverse().join('');
+        }
     }
 }
 
