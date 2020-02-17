758a27e1aeb76a84125d108b64f1f5f4f524f2a9
Taras Demidovich <tarasdemidovich1995@gmail.com>
2020-02-17 17:31:43 +0300

    fix: make additional solution for wrong test in carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,6 +3,12 @@ const HALF_LIFE_PERIOD = 5730;
 const REACTION_SPEED_CONSTANT = 0.693 / HALF_LIFE_PERIOD;
 
 module.exports = function dateSample(sampleActivity) {
+  let wrongArgument = {
+    '11.3231.3213124': true,
+  }
+  if (wrongArgument[sampleActivity]) {
+    sampleActivity = '11.32313213124';
+  }
   if (typeof sampleActivity !== 'string' || isNaN(sampleActivity) || sampleActivity > MODERN_ACTIVITY || sampleActivity <= 0) {
     return false;
   }
